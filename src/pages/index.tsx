ae537eab8297fe79399510a3f09e8dbaa0914727
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-05-09 20:20:21 -0300

    add home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { styled } from '../styles';
+
+const Button = styled('button', {
+  backgroundColor: '$green300',
+  color: 'white',
+  border: 0,
+  borderRadius: 4,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+});
+
+export default function Home() {
+  return <Button>Enviar</Button>;
+}

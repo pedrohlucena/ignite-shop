20e220c46fe08b9cc7d41766b1a34206a7e8dbe6
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-05-09 21:56:30 -0300

    add pages header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,18 @@ import { globalStyles } from '../styles/global';
 
 globalStyles();
 
+import Image from 'next/image';
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }

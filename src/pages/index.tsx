44663b1bfb1512c12ce4a72f5f606893d4e25698
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-05-11 19:47:46 -0300

    add carrosel to products listing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
 import Image from 'next/image';
 import tshirt1 from '../assets/camisetas/1.png';
 import tshirt2 from '../assets/camisetas/2.png';
+import tshirt3 from '../assets/camisetas/3.png';
 import { HomeContainer, Product } from '../styles/pages/home';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={tshirt1} width={520} height={480} alt="" />
 
         <footer>
@@ -15,8 +25,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={tshirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tshirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tshirt3} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>

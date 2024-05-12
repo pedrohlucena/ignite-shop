ae4f4eb1e964e9ef041c5d6dc55197fbe3ec0afe
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-05-12 18:18:02 -0300

    add SSG to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from 'keen-slider/react';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Stripe from 'stripe';
 import { stripe } from '../lib/stripe';
@@ -45,7 +45,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async ({}) => {
   const params = {
     expand: ['data.default_price'],
   };
@@ -65,5 +65,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   };
 };
